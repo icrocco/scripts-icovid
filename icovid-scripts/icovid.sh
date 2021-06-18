1d536387246170e8d717529685993febe91fef0d
Pascual Soto Uribe <pascualsu@gmail.com>
2021-06-18 16:01:40 -0400

    New update 1624046500

--- icovid-scripts/icovid.sh
+++ icovid-scripts/icovid.sh
@@ -15,11 +15,12 @@ echo "[`(date +"%F %T")`] Creación carpeta /archivos-step-one/${EXECUTIONDATE}/
 echo "[`(date +"%F %T")`] Creación carpeta /archivos-step-two/${EXECUTIONDATE}/"
 
 # # cd /home/pas/python/icovid-scripts;
-# mkdir /home/vale/datagov/icovid/scripts/archivos-step-one/${EXECUTIONDATE};
-# mkdir /home/vale/datagov/icovid/scripts/archivos-step-two/${EXECUTIONDATE};
-cd /home/vale/datagov/icovid/scripts;
-mkdir /home/vale/datagov/icovid/scripts/archivos-step-one/${EXECUTIONDATE};
-mkdir /home/vale/datagov/icovid/scripts/archivos-step-two/${EXECUTIONDATE};
+# mkdir /home/vale/datagov/icovid/scripts/scripts-icovid/icovid-scripts/archivos-step-one/${EXECUTIONDATE};
+# mkdir /home/vale/datagov/icovid/scripts/scripts-icovid/icovid-scripts/archivos-step-two/${EXECUTIONDATE};
+
+cd /home/vale/datagov/icovid/scripts/scripts-icovid/icovid-scripts/scripts-icovid/icovid-scripts;
+mkdir /home/vale/datagov/icovid/scripts/scripts-icovid/icovid-scripts/scripts-icovid/icovid-scripts/archivos-step-one/${EXECUTIONDATE};
+mkdir /home/vale/datagov/icovid/scripts/scripts-icovid/icovid-scripts/scripts-icovid/icovid-scripts/archivos-step-two/${EXECUTIONDATE};
 
 echo "[`(date +"%F %T")`] Ejecutamos el py script que generas los archivos para el sitio ICOVID"
 
@@ -27,21 +28,21 @@ python3 step_two.py ${EXECUTIONDATE}
 
 echo "[`(date +"%F %T")`] Copiamos los archivos generados para el sitio ICOVID en la ruta /backup/generated/"
 
-# /home/vale/datagov/icovid/scripts/backup/generated/
+# /home/vale/datagov/icovid/scripts/scripts-icovid/icovid-scripts/backup/generated/
 if [ ! -d "${PATHDOCVALE}/ICOVID/resumen/${EXECUTIONDATEBACKUP}" ]; then
-  mkdir /home/vale/datagov/icovid/scripts/backup/generated/${EXECUTIONDATEBACKUP};
-  cp /home/vale/datagov/icovid/scripts/archivos-step-one/${EXECUTIONDATE}/*.xlsx /home/vale/datagov/icovid/scripts/backup/generated/${EXECUTIONDATEBACKUP};
-  cp /home/vale/datagov/icovid/scripts/archivos-step-two/${EXECUTIONDATE}/*.xlsx /home/vale/datagov/icovid/scripts/backup/generated/${EXECUTIONDATEBACKUP};
+  mkdir /home/vale/datagov/icovid/scripts/scripts-icovid/icovid-scripts/scripts-icovid/icovid-scripts/backup/generated/${EXECUTIONDATEBACKUP};
+  cp /home/vale/datagov/icovid/scripts/scripts-icovid/icovid-scripts/archivos-step-one/${EXECUTIONDATE}/*.xlsx /home/vale/datagov/icovid/scripts/scripts-icovid/icovid-scripts/backup/generated/${EXECUTIONDATEBACKUP};
+  cp /home/vale/datagov/icovid/scripts/scripts-icovid/icovid-scripts/archivos-step-two/${EXECUTIONDATE}/*.xlsx /home/vale/datagov/icovid/scripts/scripts-icovid/icovid-scripts/backup/generated/${EXECUTIONDATEBACKUP};
 else
-  mkdir /home/vale/datagov/icovid/scripts/backup/generated/${EXECUTIONDATEBACKUP}_${UNIXTIME};
-  mv /home/vale/datagov/icovid/scripts/backup/generated/${EXECUTIONDATEBACKUP}/* /home/vale/datagov/icovid/scripts/backup/generated/${EXECUTIONDATEBACKUP}_${UNIXTIME};
-  cp /home/vale/datagov/icovid/scripts/archivos-step-one/${EXECUTIONDATE}/*.xlsx /home/vale/datagov/icovid/scripts/backup/generated/${EXECUTIONDATEBACKUP};
-  cp /home/vale/datagov/icovid/scripts/archivos-step-two/${EXECUTIONDATE}/*.xlsx /home/vale/datagov/icovid/scripts/backup/generated/${EXECUTIONDATEBACKUP};
+  mkdir /home/vale/datagov/icovid/scripts/scripts-icovid/icovid-scripts/backup/generated/${EXECUTIONDATEBACKUP}_${UNIXTIME};
+  mv /home/vale/datagov/icovid/scripts/scripts-icovid/icovid-scripts/backup/generated/${EXECUTIONDATEBACKUP}/* /home/vale/datagov/icovid/scripts/scripts-icovid/icovid-scripts/backup/generated/${EXECUTIONDATEBACKUP}_${UNIXTIME};
+  cp /home/vale/datagov/icovid/scripts/scripts-icovid/icovid-scripts/archivos-step-one/${EXECUTIONDATE}/*.xlsx /home/vale/datagov/icovid/scripts/scripts-icovid/icovid-scripts/backup/generated/${EXECUTIONDATEBACKUP};
+  cp /home/vale/datagov/icovid/scripts/scripts-icovid/icovid-scripts/archivos-step-two/${EXECUTIONDATE}/*.xlsx /home/vale/datagov/icovid/scripts/scripts-icovid/icovid-scripts/backup/generated/${EXECUTIONDATEBACKUP};
 fi
 
 echo "[`(date +"%F %T")`] Copiamos las carpetas de interés del repo actualizadas en la ruta /backup/repo/"
 
-# /home/vale/datagov/icovid/scripts/backup/repo/
+# /home/vale/datagov/icovid/scripts/scripts-icovid/icovid-scripts/backup/repo/
 if [ ! -d "${PATHDOCVALE}/ICOVID/icovid-repo/${EXECUTIONDATEBACKUP}" ]; then
   mkdir ${PATHDOCVALE}/ICOVID/icovid-repo/${EXECUTIONDATEBACKUP};
   cp -r /home/vale/datagov/icovid/ICOVID/dimension1 ${PATHDOCVALE}/ICOVID/icovid-repo/${EXECUTIONDATEBACKUP};
