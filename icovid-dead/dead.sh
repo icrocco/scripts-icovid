b962724d763b8283d140ea63114e2c88e85df34c
GOB D+I <49956210+datagovuc@users.noreply.github.com>
2021-09-28 18:26:03 -0300

    Update dead.sh

--- icovid-dead/dead.sh
+++ icovid-dead/dead.sh
@@ -2,8 +2,8 @@
 
 set -e;
 
-#FILEDATE=$(date --date="last thursday" +%d%m%Y);
-FILEDATE="16092021";
+FILEDATE=$(date --date="last thursday" +%d%m%Y);
+#FILEDATE="16092021";
 TODAY=$(date +%Y%m%d);
 MONTH=$(date +%m);
 #MONTH="08";
