0618120a1c1d3e58efe3c22c5116c05e4a01ff9b
GOB D+I <49956210+datagovuc@users.noreply.github.com>
2022-01-20 16:18:20 -0300

    Update dead.sh

--- icovid-dead/dead.sh
+++ icovid-dead/dead.sh
@@ -2,8 +2,8 @@
 
 set -e;
 
-#FILEDATE=$(date --date="last thursday" +%d%m%Y);
-FILEDATE="07012022";
+FILEDATE=$(date --date="last thursday" +%d%m%Y);
+#FILEDATE="07012022";
 TODAY=$(date +%Y%m%d);
 MONTH=$(date +%m);
 #MONTH="08";
